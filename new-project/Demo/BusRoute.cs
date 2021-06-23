d4f8b51c66083079fc16b0d6cd73c2114154104e
TheyCallMeJQ <jaquesmyster@hotmail.com>
2021-06-23 16:49:52 -0400

    Project start

---
+++ new-project/Demo/BusRoute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Demo
+{
+    public class BusRoute
+    {
+        public int Route { get; }
+        public string Destination { get; }
+        public string Source { get; }
+        public BusRoute(int route, string source, string destination)
+        {
+            this.Route = route;
+            this.Source = source;
+            this.Destination = destination;
+        }
+
+        public override string ToString() => $"{this.Route}: {this.Source} -> {this.Destination}";
+    }
+}
---
+++ new-project/Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusRoute route40 = new BusRoute(route: 40, source: "Morecambe", destination: "Preston");
+            BusRoute route42 = new BusRoute(route: 42, source: "Lancaster", destination: "Blackpool");
+            BusRoute[] routes = {
+                route40, route42
+            };
+            BusRoute[] relevantRoutes = routes.Where(r => r.Destination.ToLower() == "blackpool").ToArray();
+            foreach (var rr in relevantRoutes)
+            {
+                Console.WriteLine($"{rr}");
+            }
+        }
+    }
+}
