4f747594c7129d116419f80f61960731f909c81a
TheyCallMeJQ <jaquesmyster@hotmail.com>
2021-06-25 11:22:15 -0400

    Implemented Dictionary bus route demo

--- new-project/Demo/BusRepository.cs
+++ new-project/Demo/BusRepository.cs
@@ -22,22 +22,25 @@ namespace Demo
         //     };
         // }
 
-        public static List<BusRoute> InitializeRoutes()
+        public static Dictionary<int, BusRoute> InitializeRoutes()
         {
-            return new List<BusRoute>{
-                new BusRoute(route: 40, locationsServed: new string[]{"Morecambe", "Lancaster", "Garstang", "Preston"}),
-                new BusRoute(route: 42, locationsServed: new string[]{
+            return new Dictionary<int, BusRoute>(){
+                {40, new BusRoute(route: 40, locationsServed: new string[]{"Morecambe", "Lancaster", "Garstang", "Preston"})},
+                {42, new BusRoute(route: 42, locationsServed: new string[]{
                     "Lancaster", "Garstang", "Blackpool"
-                }),
-                new BusRoute(route: 100, locationsServed: new string[]{
+                })},
+                {100, new BusRoute(route: 100, locationsServed: new string[]{
                     "University", "Lancaster", "Morecambe"
-                }),
-                new BusRoute(route: 555, locationsServed: new string[]{
+                })},
+                {555, new BusRoute(route: 555, locationsServed: new string[]{
                      "Lancaster", "Carnforth", "Kendal", "Windermere", "Keswick"
-                }),
-                new BusRoute(route: 69, locationsServed: new string[]{
+                })},
+                {5, new BusRoute(route: 5, locationsServed: new string[]{
+                    "Overton", "Morecambe", "Carnforth"
+                })},
+                {69, new BusRoute(route: 69, locationsServed: new string[]{
                     "this", "is", "test", "data"
-                }),
+                })},
             };
         }
     }
--- new-project/Demo/Program.cs
+++ new-project/Demo/Program.cs
@@ -9,33 +9,33 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-            List<BusRoute> routes = BusRepository.InitializeRoutes();
-            // string destination = "Lancaster";
-            // List<BusRoute> relevantRoutes = FindBusesTo(destination, routes);
-            // Console.WriteLine($"Routes to: {destination}");
-            // foreach (var rr in relevantRoutes)
-            // {
-            //     Console.WriteLine($"{rr}");
-            // }
-
-            Console.WriteLine("All routes - Before removal");
-            foreach (var r in routes)
+            Dictionary<int, BusRoute> routes = BusRepository.InitializeRoutes();
+            Console.Write("Which route would you like? ");
+            int routeNo = int.Parse(Console.ReadLine());
+            if (routes.ContainsKey(routeNo))
             {
-                Console.WriteLine($"{r}");
+                Console.WriteLine($"You've selected: {routes[routeNo]}");
             }
-            routes.RemoveAll(r => r.LocationsServed.Any(l => l.Contains("test")));
-            Console.WriteLine("All routes - After removal of test data");
-            foreach (var r in routes)
+            else
             {
-                Console.WriteLine($"{r}");
+                Console.WriteLine("That route doesn't exist :(");
             }
         }
 
 
-
-        public static List<BusRoute> FindBusesTo(string destination, List<BusRoute> routes)
+        public static void PrintRoutes(Dictionary<int, BusRoute> routes)
         {
-            return routes.FindAll(r => r.Serves(destination));
+            Console.WriteLine("All routes");
+            foreach (var entry in routes)
+            {
+                BusRoute route = entry.Value;
+                Console.WriteLine($"{route}");
+            }
         }
+
+        // public static List<BusRoute> FindBusesTo(string destination, List<BusRoute> routes)
+        // {
+        //     return routes.FindAll(r => r.Serves(destination));
+        // }
     }
 }
